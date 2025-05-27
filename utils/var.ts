e3727a37a928c0560ccc96a42529c1a926f585a1
Andi Montilla <anmdev32@gmail.com>
2025-05-27 13:43:02 -0300

    first commit

---
+++ utils/var.ts
@@ -0,0 +1,7 @@
+const APPNAME     = process.env.NEXT_PUBLIC_APPNAME || 'Next.js App'
+const DESCRIPTION = process.env.NEXT_PUBLIC_DESCRIPTION || 'A Next.js application using Tanstack Query'
+
+export {
+  APPNAME,
+  DESCRIPTION,
+}
