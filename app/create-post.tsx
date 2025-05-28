543e5ad69502538124f15b5af3365feb594dbe41
SugeyCh <chaconsugey2003@gmail.com>
2025-05-27 21:40:25 -0300

    Using mutate to do a basic post and put/patch with tanstask-query

--- app/create-post.tsx
+++ app/create-post.tsx
@@ -1,5 +1,69 @@
+import { useState } from "react"
+import { useMutation } from "@tanstack/react-query"
+import { useQueryClient } from "@tanstack/react-query"
+
+const create = async (data: any) => {
+  const r = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+
+  return r.json()
+}
+
 export function CreatePost() {
+  const [ title, setTitle ] = useState("")
+  const query = useQueryClient()
+
+  const { mutate } = useMutation({ 
+    mutationFn: create,
+    // onSuccess: () => {
+      // esto se ejecuta cuando el query es exitoso
+      // con el invalidateQueries esto se obliga a que traiga de nuevo la data de la api
+      // en cambio, con el otro metodo (refetch) se ejecuta trayendo toda la data de nuevo, cuando detecta que hubo un cambio
+    //   query.invalidateQueries({ queryKey: ['posts'] })
+    // },
+    onMutate: async (newData: any) => {
+      // cancela las queries que podrían estar para que después de hacer el post, se le añada esa nueva data al get
+      // de modo que se hace un copy de todo lo que trae el get de la api (que es lo que demuestra el "old") y se le añade la nueva data que se acaba de añadir en el post
+      await query.cancelQueries({ queryKey: ['posts'] })
+      const previousPost = query.getQueryData(['posts'])
+      query.setQueryData(['posts'], (old: any) => [
+        ...old,
+        { id: Date.now(), ...newData }
+      ])
+
+      return { previousPost }
+    },
+    onError: (error, newData, context) => {
+      query.setQueryData(['posts'], context?.previousPost)
+    }
+  })
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+
+    // al usar mutate, se le pasa un valor
+    // también está el mutateAsync que igualmente se le pasa un valor pero este va a ser asincrono, va a esperar
+    mutate({
+      title,
+      body: 'This is a new post'
+    })
+  }
+
   return (
-    <div>Create!</div>
+    <div className="flex w-full justify-center mt-5">
+      <form className="flex flex-col h-41 justify-center items-center w-66 gap-3 bg-neutral-500/10 rounded-md p-3">
+        <h1 className="mb-2 text-gray-300/70 font-medium">Form</h1>
+        <input 
+          type="text" 
+          placeholder="Post title..." 
+          onChange={(e) => setTitle(e.target.value)}
+          className="outline-none h-8 text-gray-300 bg-zinc-400/12 p-2 pl-3 rounded-sm w-full"
+        />
+        <button onClick={ handleSubmit } className="bg-blue-950 text-gray-300 font-medium p-2 w-full rounded-md cursor-pointer">Create</button>
+      </form>
+    </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 'use client'
 
 import { useState } from 'react'
+import { CreatePost } from './create-post'
 import { GetAPIComponent } from './posts'
-import { GetID } from './posts-id'
+// import { GetID } from './posts-id'
 
 export default function Home() {
   const [ isMounted, setIsMounted ] = useState(false)
 
   return(
     <>
-      <button className='p-4 bg-gray-500/30 rounded-b-md' onClick={() => setIsMounted((prev) => !prev) }>Toggle</button>
+      {/* <button className='p-4 bg-gray-500/30 rounded-b-md' onClick={() => setIsMounted((prev) => !prev) }>Toggle</button>
       { isMounted && <GetAPIComponent /> }
-      <GetID id={ 3 } />
+      <GetID id={ 3 } /> */}
+
+      <CreatePost />
+      <GetAPIComponent />
     </>
   )
 }
